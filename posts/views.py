6352e70b7c8a9455ad5a8e1090ddf506296ca5d4
feliciathebeen <feliciathebeen@gmail.com>
2024-09-12 19:51:51 +0900

    dev 브랜치 ㄱ가져옴

--- posts/views.py
+++ posts/views.py
@@ -74,4 +74,4 @@ class PostDeleteUpdateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUES      
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUES)
