5bf8d94e0f83757e21a804cbcd899330b10a74ae
crk <kcl9769@naver.com>
2024-09-12 17:42:43 +0900

    feat:비밀번호 변경 구현 1. accounts/views.py PasswordResetAPIView 클래스에 비밀번호 변경 구현 2. accounts/urls.py에 password-reset path추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.UserCreateAPIView().as_view(), name="signup"),
     path("signin/", views.UserSigninAPIView.as_view()),
     path("signout/", views.UserSignoutAPIView.as_view(), name="signout"),
+    path("password-reset/", views.PasswordResetAPIView.as_view(), name="password-reset"),
     path("<str:username>/", views.ProfileDetailAPIView.as_view(), name="profile"),
 ]
