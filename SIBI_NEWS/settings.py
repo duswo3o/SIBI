e29139ec7d6943b3f602a830c8952741cc26b552
feliciathebeen <feliciathebeen@gmail.com>
2024-09-12 11:30:58 +0900

    feat: post_list
    게시글 목록보기

--- SIBI_NEWS/settings.py
+++ SIBI_NEWS/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #third-party
-    'django_extensions'
+    'django_extensions',
+    'django_seed',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import PostListAPIView
 
-app_name = 'posts'
+app_name = "posts"
 urlpatterns = [
-
+    path("", PostListAPIView.as_view(), name="post_list"),
 ]
