cbcc65c4c2a01df97cd64040e0c8fb29a04b4047
junho <willight1@naver.com>
2024-09-12 14:23:56 +0900

    feat:Update -게시글 수정 -어ì¹ACCOUNTS 완료시 요청자 확인 주석 해제 예정

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,6 @@ from .views import PostDeleteAPIView, PostUpdateAPIView
 app_name = 'posts'
 urlpatterns = [
     path('posts/<int:pk>/', PostDeleteAPIView.as_view(), name='post_delete'),
+    path('posts/<int:pk>/', PostUpdateAPIView.as_view(), name='post_update'),
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -22,4 +22,43 @@ class PostDeleteAPIView(APIView):
         post.delete()
         return Response({"detail": "게시글이 삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
 
+class PostUpdateAPIView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk, format=None):
+        post = get_object_or_404(Post, pk=pk)
+
+        # # 요청자가 작성자인지 확인
+        # if post.author != request.user:
+        #     return Response({"detail": "수정 권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+
+        # 전체 데이터를 업데이트하는 시리얼라이저
+        serializer = PostSerializer(post, data=request.data)
+
+        # 유효성 검사 후 저장
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        post = get_object_or_404(Post, pk=pk)
+
+        # # 요청자가 작성자인지 확인
+        # if post.author != request.user:
+        #     return Response({"detail": "수정 권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+
+        # 부분 데이터를 업데이트하는 시리얼라이저
+        serializer = PostSerializer(post, data=request.data, partial=True)
+
+        # 유효성 검사 후 저장
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
